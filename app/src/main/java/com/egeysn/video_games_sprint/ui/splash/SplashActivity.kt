12cd0ebadb67226be27ba56ce8fd50e37e995ad4
egeysn <yasin.ege@bil.omu.edu.tr>
2022-05-11 12:05:30 +0300

    Add view model library

--- app/src/main/java/com/egeysn/video_games_sprint/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/egeysn/video_games_sprint/ui/splash/SplashActivity.kt
@@ -1,9 +1,12 @@
 package com.egeysn.video_games_sprint.ui.splash
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.egeysn.video_games_sprint.R
 import com.naylalabs.kotlinbaseproject.common.BaseActivity
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : BaseActivity() {
     private val viewModel: SplashActivityViewModel by viewModels()
 
