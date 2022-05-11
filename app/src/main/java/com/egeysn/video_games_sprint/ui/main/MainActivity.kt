6b3640114ce1112819f73612d52ece90e837deb5
egeysn <yasin.ege@bil.omu.edu.tr>
2022-05-11 12:25:57 +0300

    Change theme

--- app/src/main/java/com/egeysn/video_games_sprint/ui/main/MainActivity.kt
+++ app/src/main/java/com/egeysn/video_games_sprint/ui/main/MainActivity.kt
@@ -14,8 +14,7 @@ class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
-        showLoading()
+        setContentView(R.layout.activity_main)
     }
 
     companion object {
